530027afafc0dd642fb297a009d874b4f3a4b90f
Junior Streichan <juniorstreichan.dev@gmail.com>
2019-06-03 08:31:21 -0400

    Aula 13 - Configurando testes de integração na API HTTP

--- src/main/java/com/tdd/exemplo/tddspringboot/repository/helper/IPessoaRepositoryQueries.java
+++ src/main/java/com/tdd/exemplo/tddspringboot/repository/helper/IPessoaRepositoryQueries.java
@@ -2,9 +2,11 @@ package com.tdd.exemplo.tddspringboot.repository.helper;
 
 import com.tdd.exemplo.tddspringboot.domain.Pessoa;
 import com.tdd.exemplo.tddspringboot.repository.filtros.PessoaFiltro;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface IPessoaRepositoryQueries {
 
     List<Pessoa> filtrar(PessoaFiltro filtro);
